585ef51cf034b726f40030553c7698ab32fd217b
alaarab <alaarab@gmail.com>
2024-09-14 18:03:37 -0700

    Add Lightbox

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,19 +1,42 @@
 
 'use client'; // Add this line
-import React from 'react';
+import React, { useState } from 'react';
 import Gallery from 'react-photo-gallery';
 import Photo from './Photo';
+import 'react-image-lightbox/style.css'; // This only needs to be imported once in your app
 import { photos } from '../data/photos';
+import Lightbox from 'react-image-lightbox';
 
 const PhotoGallery = () => {
   const allPhotos = Object.values(photos).flat();
+  const [photoIndex, setPhotoIndex] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openLightbox = (event, { index }) => {
+    setPhotoIndex(index);
+    setIsOpen(true);
+  };
 
   return (
-    <Gallery
-      photos={allPhotos}
-      margin={5}
-      renderImage={Photo}
-    />
+    <div>
+      <Gallery photos={allPhotos} onClick={openLightbox} />
+      {isOpen && (
+        <Lightbox
+          mainSrc={allPhotos[photoIndex].src}
+          nextSrc={allPhotos[(photoIndex + 1) % allPhotos.length].src}
+          prevSrc={allPhotos[(photoIndex + allPhotos.length - 1) % allPhotos.length].src}
+          imageTitle={`Image ${photoIndex + 1}`}
+          imageCaption={`Dimensions: ${allPhotos[photoIndex].width} x ${allPhotos[photoIndex].height}`}
+          onCloseRequest={() => setIsOpen(false)}
+          onMovePrevRequest={() =>
+            setPhotoIndex((photoIndex + allPhotos.length - 1) % allPhotos.length)
+          }
+          onMoveNextRequest={() =>
+            setPhotoIndex((photoIndex + 1) % allPhotos.length)
+          }
+        />
+      )}
+    </div>
   );
 };
 
